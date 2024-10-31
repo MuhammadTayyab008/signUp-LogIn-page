37295cb0ca860ccfee7647a582f49e7655d2e7dc
Muhammad Taayab <maliktayyabbro@gmmail.com>
2024-10-31 09:15:07 +0500

    first commit

---
+++ signUp-LogIn-page/signUp_LogIn_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  signUp_LogIn_pageApp.swift
+//  signUp-LogIn-page
+//
+//  Created by Rashdan Natiq on 2024-10-31.
+//
+
+import SwiftUI
+
+@main
+struct signUp_LogIn_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
